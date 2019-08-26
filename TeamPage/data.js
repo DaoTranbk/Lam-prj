73f2c17dafa2d30303e8191838cc84a1f3c9ffae
DaoTranbk <53143326+DaoTranbk@users.noreply.github.com>
2019-08-26 11:00:44 +0700

    Add files via upload

---
+++ TeamPage/data.js
@@ -0,0 +1,19 @@
+const TeamTasks = [
+    { id: "1", value: "hoc" },
+    { id: "2", value: "ngu" },
+    { id: "3", value: "choi" },
+    { id: "4", value: "the thao" },
+];
+
+const Users = [
+    { id: "1", name: "Thai Chien", isAdmin: "false"},
+    { id: "2", name: "Cong Dao", isAdmin: "false"},
+    { id: "3", name: "Tung Lam", isAdmin: "true"},
+    { id: "4", name: "Quang Phuc", isAdmin: "false"},
+];
+
+const AssignedTasks = [
+    { id: "", taskId: "", userId: "" }
+];
+
+export {TeamTasks, Users, AssignedTasks};
